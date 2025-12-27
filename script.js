fe1e03a1377e2f8f31d52763c4f942d15f616fcf
NeevJain482 <neevjain482.code@gmail.com>
2025-12-28 02:47:19 +0530

    Sticky notes can be added

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("sticky-notes");
+const topicInput = document.getElementById("topic-input");
+const textArea = document.getElementById("text-area");
+const template = document.getElementById("template");
+const addBtn = document.getElementById("add-btn");
+
+let count = 0;
+
+const handleClick = () => {
+  let topic = topicInput.value.trim();
+  let text = textArea.value.trim();
+  if (!topic && !text) return;
+  count++;
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".topic").innerText = topic;
+  clone.querySelector(".text").innerText = text;
+  clone.querySelector(".delete-btn").addEventListener("click", (e) => {
+    e.target.closest(".window").remove();
+  });
+  container.appendChild(clone);
+};
+
+addBtn.addEventListener("click", handleClick);
